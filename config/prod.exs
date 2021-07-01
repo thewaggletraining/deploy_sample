f6a422ec2b6291cab13c48df96ba5fe782684dcd
Yosuke <Yosuke@yukinoMBP.nrima1.kt.home.ne.jp>
2021-07-01 17:45:32 +0900

    add prod check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :deploy_sample, DeploySampleWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://this-innocent-binturong.gigalixirapp.com/"]
 
 # Do not print debug messages in production
 config :logger, level: :info
